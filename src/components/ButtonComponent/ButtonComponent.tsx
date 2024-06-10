7cabac5f0ac5d420c3b344d4316ef4ced7f11c4a
Sushko Andriy <itsushkoandriy@gmail.com>
2024-06-10 18:22:53 +0300

    create button component

---
+++ src/components/ButtonComponent/ButtonComponent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import { Link } from 'gatsby';
+
+const ButtonComponent = () => {
+  return <div></div>;
+};
+
+export default ButtonComponent;
