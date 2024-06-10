d60783233f1f4558b9c8f7651615d34e7a777b97
Sushko Andriy <itsushkoandriy@gmail.com>
2024-06-10 12:23:21 +0300

    button style

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -9,24 +9,20 @@ const Hero: React.FC<HeroProps> = ({}) => {
       <div className={styles.header}>ТЕПЛО НА ПЕРЕДОВУ</div>
       <div className={styles.title}>Станьте частиною перемоги!</div>
       <div className={styles.description}>
-        Наша діяльність спрямована на допомогу захисникам і захисницям України
-        на передовій. Ми допомагаємо як окремим військовим, так і підрозділам.
+        Наша діяльність спрямована на допомогу захисникам і захисницям України на передовій. Ми допомагаємо як окремим
+        військовим, так і підрозділам.
       </div>
       <div className={styles.buttoncontainer}>
-        <a
+        {/* <a
           href="https://send.monobank.ua/jar/pAH2wwD8n"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          <button className={styles.filledbutton}>Підтримати</button>
-        </a>
-        <a
-          href="https://t.me/teplonaperedovu"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className={styles.outlinedbutton}>Потрібна допомога</button>
-        </a>
+        > */}
+        <button className={styles.filledbutton}>Підтримати</button>
+        {/* </a> */}
+        {/* <a href="https://t.me/teplonaperedovu" target="_blank" rel="noopener noreferrer"> */}
+        <button className={styles.outlinedbutton}>Потрібна допомога</button>
+        {/* </a> */}
       </div>
     </section>
   );
