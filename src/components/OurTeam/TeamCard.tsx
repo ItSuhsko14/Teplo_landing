f35d1e237b96ed04302661c7e59c6a81b21fba27
ItSushko14 <itsushkoandriy@gmail.com>
2024-06-16 11:37:01 +0300

    our team change composition

--- src/components/OurTeam/TeamCard.tsx
+++ src/components/OurTeam/TeamCard.tsx
@@ -11,8 +11,8 @@ interface TeamCardProps {
 const TeamCard: React.FC<TeamCardProps> = ({ name, img, text, button }) => {
   return (
     <div className={styles.cardContainer}>
-      <h2 className={styles.cardTitle}>{name}</h2>
       <img src={img} alt="123" className={styles.cardImg} />
+      <h2 className={styles.cardTitle}>{name}</h2>
       <p className={styles.role}>{text}</p>
     </div>
   );
