9e48e176f4f5ed919667473b402ccae513b62a55
ItSushko14 <itsushkoandriy@gmail.com>
2024-06-22 19:22:40 +0300

    new text for detailed

--- src/components/Detailed/Detailed.tsx
+++ src/components/Detailed/Detailed.tsx
@@ -57,17 +57,10 @@ const Detailed: React.FC<DetailedProps> = ({}) => {
           <p>"Тепло на передову"</p>
         </div>
         <div className={`${styles.description} ${styles.firstDescription}`}>
-          Lorem ipsum dolor sit amet consectetur. Feugiat euismod sit odio in. Diam quam condimentum ultrices in. Magna
-          in consequat lacus neque scelerisque ut tempor purus. Accumsan a mauris lacus vulputate nulla condimentum
-          massa sed lectus. Eu ut ipsum sit pretium magna. Sapien amet purus proin massa ipsum nunc ultrices auctor.
-          Porttitor blandit at orci blandit fermentum neque sapien enim. Tristique donec adipiscing semper lacus
-          adipiscing. Massa adipiscing euismod duis velit sed volutpat leo tellus. Faucibus nisl cras nullam auctor
-          tincidunt bibendum. Sollicitudin turpis lectus enim volutpat. Mauris varius id interdum venenatis euismod nunc
-          nunc ac amet. Pharetra in nullam at velit neque id purus. Aliquet felis risus viverra magna condimentum
-          aenean. Natoque feugiat euismod sapien in vulputate. Arcu dui faucibus nunc euismod orci tempor enim egestas.
-          Est nisi eget donec libero eget vel feugiat nam. Urna sapien faucibus feugiat et integer dictum mauris urna.
-          Aliquam semper a diam laoreet magna neque maecenas. Turpis posuere bibendum nunc at adipiscing diam euismod.
-          Ipsum molestie nulla volutpat proin quis platea sed ut. Id bibendum varius lorem pretium sagittis et turpis.
+          Проєкт "Тепло на передову" виник на початку повномаштабного вторгнення і спрямований на забезпечення
+          захисників всім необхідним. Почавши з постачання буржуєк на фронт, він швидко розширився і охопив значно
+          більший перелік позицій допомоги, включаючи кавери, м'які ноші, плащі-палатки, газові горілки та балони,
+          окопні свічки та інше необхідне обладнання.
         </div>
       </section>
       <div className={styles.photoSlider}>
@@ -85,17 +78,10 @@ const Detailed: React.FC<DetailedProps> = ({}) => {
       </div>
       <section className={styles.container}>
         <div className={`${styles.description} ${styles.secondDescription}`}>
-          Lorem ipsum dolor sit amet consectetur. Feugiat euismod sit odio in. Diam quam condimentum ultrices in. Magna
-          in consequat lacus neque scelerisque ut tempor purus. Accumsan a mauris lacus vulputate nulla condimentum
-          massa sed lectus. Eu ut ipsum sit pretium magna. Sapien amet purus proin massa ipsum nunc ultrices auctor.
-          Porttitor blandit at orci blandit fermentum neque sapien enim. Tristique donec adipiscing semper lacus
-          adipiscing. Massa adipiscing euismod duis velit sed volutpat leo tellus. Faucibus nisl cras nullam auctor
-          tincidunt bibendum. Sollicitudin turpis lectus enim volutpat. Mauris varius id interdum venenatis euismod nunc
-          nunc ac amet. Pharetra in nullam at velit neque id purus. Aliquet felis risus viverra magna condimentum
-          aenean. Natoque feugiat euismod sapien in vulputate. Arcu dui faucibus nunc euismod orci tempor enim egestas.
-          Est nisi eget donec libero eget vel feugiat nam. Urna sapien faucibus feugiat et integer dictum mauris urna.
-          Aliquam semper a diam laoreet magna neque maecenas. Turpis posuere bibendum nunc at adipiscing diam euismod.
-          Ipsum molestie nulla volutpat proin quis platea sed ut. Id bibendum varius lorem pretium sagittis et turpis.
+          Проєкт "Тепло на передову" був створений як відповідь на різкі потреби фронту під час військових дій. Він
+          постачає захисникам широкий спектр необхідного обладнання, починаючи від каверів і м'яких нош до
+          плащів-палаток і газових горілок. Цей проєкт є важливим елементом підтримки бойових дій і допомагає
+          забезпечити комфорт і безпеку військовим у важкі часи.
         </div>
       </section>
       <Gallery1 />
